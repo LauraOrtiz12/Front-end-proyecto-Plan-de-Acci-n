d85dfd62e2c26d98f09f1c72d71a03b01061decc
Miguel Ángel López L. <malopezl0512@gmail.com>
2024-05-15 16:09:31 -0500

    Fix

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -8,96 +8,36 @@ import InputLabel from "@/Components/InputLabel.vue";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
 import TextInput from "@/Components/TextInput.vue";
 
-defineProps({
-    canResetPassword: Boolean,
-    status: String,
-});
-
-const form = useForm({
-    email: "",
-    password: "",
-    remember: false,
-});
-
-const submit = () => {
-    form.transform((data) => ({
-        ...data,
-        remember: form.remember ? "on" : "",
-    })).post(route("login"), {
-        onFinish: () => form.reset("password"),
-    });
-};
-
-const images = [
+const IMAGES = [
   'assets/images/login-background.webp',
   'assets/images/login-background-1.webp',
 ];
 
+defineProps({
+  canResetPassword: Boolean,
+  status: String,
+});
+
+const form = useForm({
+  email: "",
+  password: "",
+  remember: false,
+});
+
+const submit = () => {
+  form.post(route("login"), {
+    onFinish: () => form.reset("password"),
+  });
+};
 </script>
 
 <template>
-    <Head title="Ingreso" />
+  <Head title="Ingreso" />
 
-    <main class="bg-no-repeat bg-cover bg-center grid grid-cols-2 w-screen h-screen">
-        <section class=""></section>
-        <section class="bg-[url('assets/images/login-background-overlay.webp')] bg-cover bg-center h-full">
-            <img :src="images[Math.floor(Math.random() * images.length)]" alt="" class="absolute -z-10 w-1/2 h-full object-cover">
-        </section>
-    </main>
-
-    <!-- <AuthenticationCard>
-        <template #logo>
-            <AuthenticationCardLogo />
-        </template>
-
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
-        </div>
-
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Correo Electrónico" />
-                <TextInput
-                    id="email"
-                    v-model="form.email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    required
-                    autofocus
-                    autocomplete="username"
-                />
-                <InputError class="mt-2" :message="form.errors.email" />
-            </div>
-
-            <div class="mt-4">
-                <InputLabel for="password" value="Clave" />
-                <TextInput
-                    id="password"
-                    v-model="form.password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    required
-                    autocomplete="current-password"
-                />
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
-
-            <div class="block mt-4">
-                <label class="flex items-center">
-                    <Checkbox v-model:checked="form.remember" name="remember" />
-                    <span class="ms-2 text-sm text-gray-600">Recordarme</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                    Perdí mi contraseña?
-                </Link>
-
-                <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                   Iniciar Sesión
-                </PrimaryButton>
-            </div>
-        </form>
-    </AuthenticationCard> -->
+  <main class="bg-no-repeat bg-cover bg-center grid grid-cols-2 w-screen h-screen">
+    <section class=""></section>
+    <section class="bg-[url('assets/images/login-background-overlay.webp')] bg-cover bg-center h-full">
+      <img :src="IMAGES[Math.floor(Math.random() * IMAGES.length)]" alt="" class="absolute -z-10 w-1/2 h-full object-cover">
+    </section>
+  </main>
 </template>
