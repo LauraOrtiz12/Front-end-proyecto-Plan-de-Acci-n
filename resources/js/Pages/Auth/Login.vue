9897cef088e57467a08b92f173b83d1ec06f6a79
Miguel Ángel López L. <malopezl0512@gmail.com>
2024-05-15 16:28:31 -0500

    Fix

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -9,35 +9,50 @@ import PrimaryButton from "@/Components/PrimaryButton.vue";
 import TextInput from "@/Components/TextInput.vue";
 
 const IMAGES = [
-  'assets/images/login-background.webp',
-  'assets/images/login-background-1.webp',
+    "assets/images/login-background.webp",
+    "assets/images/login-background-1.webp",
 ];
 
 defineProps({
-  canResetPassword: Boolean,
-  status: String,
+    canResetPassword: Boolean,
+    status: String,
 });
 
 const form = useForm({
-  email: "",
-  password: "",
-  remember: false,
+    email: "",
+    password: "",
+    remember: false,
 });
 
 const submit = () => {
-  form.post(route("login"), {
-    onFinish: () => form.reset("password"),
-  });
+    form.post(route("login"), {
+        onFinish: () => form.reset("password"),
+    });
 };
 </script>
 
 <template>
-  <Head title="Ingreso" />
+    <Head title="Ingreso" />
 
-  <main class="bg-no-repeat bg-cover bg-center grid grid-cols-2 w-screen h-screen">
-    <section class=""></section>
-    <section class="bg-[url('assets/images/login-background-overlay.webp')] bg-cover bg-center h-full">
-      <img :src="IMAGES[Math.floor(Math.random() * IMAGES.length)]" alt="" class="absolute -z-10 w-1/2 h-full object-cover">
-    </section>
-  </main>
+    <main
+        class="bg-no-repeat bg-cover bg-center grid grid-cols-2 w-screen h-screen "
+    >
+        <section class=""></section>
+        <section
+            id="login-images"
+            class="bg-[url('assets/images/login-background-overlay.webp')] bg-cover bg-center h-full overflow-hidden relative hover:bg-[length:110%_110%] transition-all"
+        >
+            <img
+                :src="IMAGES[Math.floor(Math.random() * IMAGES.length)]"
+                alt=""
+                class="absolute -z-10 w-full h-full object-cover transition-all"
+            />
+        </section>
+    </main>
 </template>
+
+<style>
+    #login-images:hover img {
+        transform: scale(1.4) translate(7px, 100px);
+    }
+</style>
