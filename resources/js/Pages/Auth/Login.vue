00d4d6468f8b4ef394967a73233744507f2db7c0
Miguel Ángel López L. <malopezl0512@gmail.com>
2024-05-20 12:43:02 -0500

    Fix and update responsive

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -44,77 +44,67 @@ const selectedImage = IMAGES[Math.floor(Math.random() * IMAGES.length)];
 
     <Head title="Ingreso" />
 
-    <main class="bg-secondary bg-no-repeat bg-cover bg-center grid grid-cols-2 w-screen h-screen">
-        <section class="p-6 flex flex-col  gap-[20px] justify-center  items-center z-10">
-            <div class="flex flex-col items-center leading-[25px]">
-                <h1 style="font-size: 25px; " class="font-extrabold text-white p-2 rounded-lg">Bienvenido al</h1>
-                <span style="font-size: 25px ;"
-                    class="font-bold  text-primary flex  w-full items-center  justify-center">
+    <main class=" bg-secondary bg-no-repeat bg-cover bg-center grid xl:grid-cols-2 w-screen h-screen">
+        <section class="p-6 flex flex-col gap-5 justify-center items-center">
+            <div class="text-center leading-6">
+                <h1 class="font-extrabold text-2xl text-white p-2">Bienvenido al</h1>
+                <span class="font-bold text-2xl text-primary">
                     Plan Anual De Adquisición
                 </span>
             </div>
-            <div class="gap-[5px] p-4 w-[31.25rem] rounded-lg">
-                <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-                    {{ status }}
-                </div>
-
-                <form @submit.prevent="submit">
-                    <div>
-                        <i
-                            class="fa-solid fa-envelope absolute translate-x-[0.625rem] translate-y-[2.625rem] text-secondary"></i>
-                        <InputLabel for="email" value="Correo Electrónico" class="text-white" />
-                        <TextInput id="email" v-model="form.email" type="email" required
-                            class="mt-1 block w-full pl-8 pr-10" autofocus autocomplete="username" />
-                        <InputError class="mt-2" :message="form.errors.email" />
-                    </div>
-
-                    <div class="mt-4">
-                        <i
-                            class="fa-solid fa-key absolute translate-x-[0.625rem] translate-y-[2.625rem] text-secondary"></i>
-                        <InputLabel for="password" value="Contraseña" class="text-white" />
-                        <TextInput id="password" v-model="form.password" :type="showPassword ? 'text' : 'password'"
-                            class="mt-1 block w-full pl-8 pr-10" required autocomplete="current-password" />
-                        <i class="fa-solid absolute cursor-pointer hover:bg-gray-200 p-1 rounded-full text-secondary translate-x-[27.1rem] translate-y-[-1.97rem]"
-                            :class="showPassword ? 'fa-eye' : 'fa-eye-slash'" @click="toggleShowPassword">
-                        </i>
-                        <InputError class="mt-2" :message="form.errors.password" />
-                    </div>
-
-                    <div class="block mt-4">
-                        <label class="flex items-center">
-                            <Checkbox v-model:checked="form.remember" name="remember" />
-                            <span class="ms-1 text-[16px] text-white">Recordarme</span>
-                        </label>
-                    </div>
-
-                    <div class="flex justify-end flex-col gap-2">
-                        <Link v-if="canResetPassword" :href="route('password.request')"
-                            class="underline text-[16px] text-white hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        Perdí mi contraseña?
-                        </Link>
-
-                        <PrimaryButton class="w-full justify-center" :class="{ '': form.processing }"
-                            :disabled="form.processing">
-                            Iniciar Sesión
-                        </PrimaryButton>
-                    </div>
-                </form>
+            <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+                {{ status }}
             </div>
+
+            <form @submit.prevent="submit" class="flex flex-col gap-2 w-full max-w-md">
+                <div>
+                    <i class="fa-solid fa-envelope absolute translate-x-[62.5%] translate-y-[260%] text-secondary"></i>
+                    <InputLabel for="email" value="Correo Electrónico" class="text-white" />
+                    <TextInput id="email" v-model="form.email" type="email" required
+                        class="mt-1 block w-full pl-8 pr-10" autofocus autocomplete="username" />
+                    <InputError class="mt-2" :message="form.errors.email" />
+                </div>
+                <div class="flex flex-col">
+                    <i class="fa-solid fa-key absolute translate-x-[62.5%] translate-y-[260%] text-secondary"></i>
+                    <InputLabel for="password" value="Contraseña" class="text-white" />
+                    <TextInput id="password" v-model="form.password" :type="showPassword ? 'text' : 'password'"
+                        class="mt-1 block w-full pl-8 pr-10" required autocomplete="current-password" />
+                    <i class="fa-solid cursor-pointer relative self-end hover:bg-gray-200 p-1 rounded-full text-secondary bottom-8 right-2"
+                        :class="showPassword ? 'fa-eye' : 'fa-eye-slash'" @click="toggleShowPassword">
+                    </i>
+                </div>
+                <InputError class="mt-2" :message="form.errors.password" />
+
+                <label class="flex items-center">
+                    <Checkbox v-model:checked="form.remember" name="remember" />
+                    <span class="ms-1 text-[16px] text-white">Recordarme</span>
+                </label>
+
+                <Link v-if="canResetPassword" :href="route('password.request')"
+                    class="underline text-[16px] text-white hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                Perdí mi contraseña?
+                </Link>
+
+                <PrimaryButton class="w-full justify-center" :class="{ '': form.processing }"
+                    :disabled="form.processing">
+                    Iniciar Sesión
+                </PrimaryButton>
+            </form>
 
             <img src="assets/images/logosena.webp" alt="" />
         </section>
 
-        <section class="background-hover overflow-hidden rounded-bl-[50%]">
+        <section class="background-hover overflow-hidden rounded-bl-[50%] row-start-1 xl:row-start-auto">
             <img src="assets/images/login-background-overlay.webp" alt=""
-                class="object-cover transition-all duration-1000 relative z-10" />
+                class="object-cover transition-all duration-1000 relative z-10 w-full h-full" />
             <img :src="selectedImage" alt=""
-                class="object-cover transition-all duration-1000 relative -translate-y-full" />
+                class="object-cover order-1 transition-all duration-1000 relative -translate-y-full w-full h-full" />
         </section>
     </main>
 </template>
 
 <style>
-.background-hover:hover > :nth-child(2) {
+.background-hover:hover> :nth-child(2) {
     --tw-scale-x: 1.25;
     --tw-scale-y: 1.25;
     transform: translate(var(--tw-translate-x), var(--tw-translate-y)) rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y));
