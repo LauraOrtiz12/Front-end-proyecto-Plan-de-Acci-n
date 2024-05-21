c471b590896415e4ebd239bd9102a9309a0ffb7c
Miguel Ángel López L. <malopezl0512@gmail.com>
2024-05-21 10:38:02 -0500

    "Updated CSS class names in various Vue components to use '-default' suffix"

--- resources/js/Components/ResponsiveNavLink.vue
+++ resources/js/Components/ResponsiveNavLink.vue
@@ -10,8 +10,8 @@ const props = defineProps({
 
 const classes = computed(() => {
     return props.active
-        ? 'block w-full ps-3 pe-4 py-2 text-start text-base font-bold text-white focus:outline-none focus:text-primary focus:bg-indigo-100 focus:border-indigo-700 transition duration-150 ease-in-out'
-        : 'block w-full ps-3 pe-4 py-2 text-start text-base font-medium text-white hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-primary focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out';
+        ? 'block w-full ps-3 pe-4 py-2 text-start text-base font-bold text-white focus:outline-none focus:text-primary-default focus:bg-indigo-100 focus:border-indigo-700 transition duration-150 ease-in-out'
+        : 'block w-full ps-3 pe-4 py-2 text-start text-base font-medium text-white hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-primary-default focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out';
 });
 </script>
 
