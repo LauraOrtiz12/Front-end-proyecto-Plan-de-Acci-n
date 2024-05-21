af9f1c291ddecad4ecd69ad76802ef4ed5ca8b5b
danifede850 <danifede850@gmail.com>
2024-05-21 11:39:57 -0500

    fix validar

--- resources/js/Pages/Validate/ValidateViability.vue
+++ resources/js/Pages/Validate/ValidateViability.vue
@@ -36,7 +36,7 @@ const goJustify = (item) => {
     <AppLayout>
         <template #header>
             <div class="flex items-center">
-                <span class="mr-2">Vigencia</span>
+                <span class="flex items-center mr-2 bg-primary-default p-2 rounded-lg text-white"><img src="assets/images/vigencia.webp" alt="" width="35px">Vigencia</span>
                 <select class="block w-40 py-1 px-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" name="viability" id="viability" v-model="validity">
                     <option :value="via.id" v-for="via in viability" :key="via.id">{{via.validity}}</option>
                 </select>
@@ -79,7 +79,9 @@ const goJustify = (item) => {
             </div>
         </div>
         <div v-else class="text-center bg-white mt-1">
-            <span>No Tiene Asociado una dependencia</span>
+            <span class="bg-gray-200 p-4 rounded-lg">No Tiene Asociado 
+                   una dependencia
+            </span>
         </div>
 
         <div class="w-full mt-4" v-if="$page.props.auth.user.role_id != 1 && Object.keys(estateIndicators).length > 0">
