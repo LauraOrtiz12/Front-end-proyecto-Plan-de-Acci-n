23bb4b0ec960e8e6f5327c23217b2ecf3c420333
danifede850 <danifede850@gmail.com>
2024-05-22 16:48:22 -0500

    "Remove max width and margin from ListUsers.vue container div"

--- resources/js/Pages/Users/ListUsers.vue
+++ resources/js/Pages/Users/ListUsers.vue
@@ -29,7 +29,7 @@ const openAssocAdviser = (user) => {
 <template>
     <AppLayout>
 
-        <div class="max-w-7xl m-auto px-3">
+        <div class="">
             <div class="py-6">
                 <button @click="newUserModal = !newUserModal"
                         class="inline-flex items-center px-4 py-2 bg-primary-default border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-secondary focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
