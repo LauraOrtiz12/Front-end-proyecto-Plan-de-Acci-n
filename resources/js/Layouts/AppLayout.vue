db5efe012f9ae372db3bf607569499282cf6a701
Miguel Ángel López L. <malopezl0512@gmail.com>
2024-05-27 14:10:33 -0500

    Added "overflow-y-auto" to nav element's class list in AppLayout.vue.

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -45,7 +45,7 @@ function toggleMenu() {
         <div class="min-h-screen bg-gray-100 flex " >
             <div :class="[menu ? 'bg-black opacity-50 h-screen lg:h-auto':'transparent', 'transition-all fixed w-screen z-10']" @click="menu ? toggleMenu():''"></div>
             <nav
-                :class="[menu ? 'fixed lg:sticky' : 'absolute -translate-x-full', 'flex z-20 transition-all bg-white shadow-md w-64 h-screen p-4 flex-col gap-4 top-0']">
+                :class="[menu ? 'fixed lg:sticky' : 'absolute -translate-x-full', 'flex z-20 transition-all bg-white shadow-md w-64 h-screen p-4 flex-col gap-4 top-0 overflow-y-auto']">
                 <Link :href="route('dashboard')">
                 <ApplicationMark class="m-auto h-10 w-auto" />
                 </Link>
