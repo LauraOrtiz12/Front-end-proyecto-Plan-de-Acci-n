babf55e4df917b94d72f5eb70108f7eb21f34e05
Miguel Ángel López L. <malopezl0512@gmail.com>
2024-06-05 15:46:08 -0500

    "Refactored ListUsers.vue: updated imports, spacing, and formatting in script and template sections."

--- resources/js/Pages/Users/ListUsers.vue
+++ resources/js/Pages/Users/ListUsers.vue
@@ -1,10 +1,10 @@
 <script setup>
 import AppLayout from "@/Layouts/AppLayout.vue";
 import Modal from "@/Components/Modal.vue";
-import {ref} from "vue";
+import { ref } from "vue";
 import Register from "@/Pages/Auth/Register.vue";
 import EditUser from "@/Pages/Auth/EditUser.vue";
-import {router} from '@inertiajs/vue3'
+import { router } from '@inertiajs/vue3'
 import ListEstatesAssoc from "@/Pages/Estate/ListEstatesAssoc.vue";
 
 defineProps({
@@ -21,13 +21,13 @@ const userSelect = ref(null);
 
 const close = () => {
     newUserModal.value = false;
-    router.reload({only: ['users']})
+    router.reload({ only: ['users'] })
 
 }
 
 const closeEdit = () => {
     editUserModal.value = false;
-    router.reload({only: ['users']})
+    router.reload({ only: ['users'] })
 
 }
 const openAssocAdviser = (user) => {
@@ -49,93 +49,98 @@ const openEditUser = (user) => {
     <AppLayout :title="pageTitle">
 
         <template #header>
-            <h1 class="font-semibold text-xl text-secondary-default my-auto">{{pageTitle}}</h1>
+            <h1 class="font-semibold text-xl text-secondary-default my-auto">{{ pageTitle }}</h1>
         </template>
 
         <div class="flex flex-col gap-4">
-                <button @click="newUserModal = !newUserModal"
-                        class="transition-all w-fit ml-auto text-white px-4 py-2 bg-secondary-default rounded-md hover:bg-primary-default hover:scale-105">
-                    <i class="fa-solid fa-user-plus mr-1"></i>
-                    Nuevo Usuario
-                </button>
+            <button @click="newUserModal = !newUserModal"
+                class="transition-all w-fit ml-auto text-white px-4 py-2 bg-secondary-default rounded-md hover:bg-primary-default hover:scale-105">
+                <i class="fa-solid fa-user-plus mr-1"></i>
+                Nuevo Usuario
+            </button>
             <div class="overflow-x-auto rounded-xl shadow-md">
-                <table class="overflow-hidden table-fixed">
+                <table class="overflow-hidden w-full">
                     <thead class="bg-secondary-default text-white uppercase text-xs tracking-wider whitespace-nowrap">
-                    <tr>
-                        <th scope="col" class="px-6 py-4">
-                            <div class="flex items-center flex-nowrap gap-3 min-w-max">
-                                <img class="align-middle" src="assets/images/ID.webp" alt="" width="35px">
-                                Código
-                            </div>
-                        </th>
-                        <th scope="col" class="px-6 py-4 min-w-fit">
-                            <div class="flex items-center flex-nowrap gap-3">
-                                <img class="align-middle" src="assets/images/nombre.webp" alt="" width="35px">
-                                Nombre
-                            </div>
-                        </th>
-                        <th scope="col" class="px-6 py-4 min-w-fit">
-                            <div class="flex items-center flex-nowrap gap-3">
-                                <img class="align-middle" src="assets/images/correo-electronico.webp" alt=""
-                                     width="35px">
-                                Correo electrónico
-                            </div>
-                        </th>
-                        <th scope="col" class="px-6 py-4 min-w-fit">
-                            <div class="flex items-center flex-nowrap gap-3">
-                                <img class="align-middle" src="assets/images/correo-electronico.webp" alt=""
-                                     width="35px">
-                                *Responsabilidad
-                            </div>
-                        </th>
-                        <th scope="col" class="px-6 py-4 min-w-fit">
-                            <div class="flex items-center flex-nowrap gap-3">
-                                <img class="align-middle" src="assets/images/correo-electronico.webp" alt=""
-                                     width="35px">
-                                Rol
-                            </div>
-                        </th>
-                        <th scope="col" class="px-6 py-4">
-                            <div class="flex items-center flex-nowrap gap-3">
-                                <img class="align-middle" src="assets/images/acciones.webp" alt="" width="35px">
-                                Acciones
-                            </div>
-                        </th>
-                    </tr>
+                        <tr>
+                            <th scope="col" class="px-6 py-4">
+                                <div class="flex items-center flex-nowrap gap-3 min-w-max">
+                                    <img class="align-middle" src="assets/images/ID.webp" alt="" width="35px">
+                                    Código
+                                </div>
+                            </th>
+                            <th scope="col" class="px-6 py-4">
+                                <div class="flex items-center flex-nowrap gap-3 min-w-max">
+                                    <img class="align-middle" src="assets/images/nombre.webp" alt="" width="35px">
+                                    Nombre
+                                </div>
+                            </th>
+                            <th scope="col" class="px-6 py-4">
+                                <div class="flex items-center flex-nowrap gap-3 min-w-max">
+                                    <img class="align-middle" src="assets/images/correo-electronico.webp" alt=""
+                                        width="35px">
+                                    Correo electrónico
+                                </div>
+                            </th>
+                            <th scope="col" class="px-6 py-4">
+                                <div class="flex items-center flex-nowrap gap-3 min-w-max">
+                                    <img class="align-middle" src="assets/images/correo-electronico.webp" alt=""
+                                        width="35px">
+                                    *Responsabilidad
+                                </div>
+                            </th>
+                            <th scope="col" class="px-6 py-4 ">
+                                <div class="flex items-center flex-nowrap gap-3 min-w-max">
+                                    <img class="align-middle" src="assets/images/correo-electronico.webp" alt=""
+                                        width="35px">
+                                    Rol
+                                </div>
+                            </th>
+                            <th scope="col" class="px-6 py-4">
+                                <div class="flex items-center flex-nowrap gap-3 min-w-max">
+                                    <img class="align-middle" src="assets/images/acciones.webp" alt="" width="35px">
+                                    Acciones
+                                </div>
+                            </th>
+                        </tr>
                     </thead>
                     <tbody class="divide-y-4 divide-white">
-                    <!-- Itera sobre cada usuario y muestra una fila en la tabla -->
-                    <tr class="divide-x-4 divide-white" v-for="user in $page.props.users" :key="user.id">
-                        <td class="bg-gray-200 px-6 py-4 whitespace-nowrap">{{ user.code }}</td>
-                        <td class="bg-gray-100 px-6 py-4 whitespace-nowrap">{{ user.name }}</td>
-                        <td class="bg-gray-200 px-6 py-4 whitespace-nowrap">{{ user.email }}</td>
-                        <td class="bg-gray-200 px-6 py-4 table-cell">
-                         <span v-for="adviserUser in user.get_adviser_office" :key="adviserUser.get_estate.cod_dep" class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 mb-2 px-2.5 py-0.5 border border-blue-400 inline-block">
-          {{ adviserUser.get_estate.cod_dep }} - {{ adviserUser.get_estate.dependence }}
-        </span>
-                        </td>
-                        <td class="bg-gray-100 px-6 py-4 whitespace-nowrap text-sm font-medium">
-                            {{ user.get_role.rol }} ({{ user.get_role.id }})
-                        </td>
-                        <td class="flex flex-col bg-gray-100 px-5 py-4 whitespace-nowrap text-sm font-medium gap-2">
+                        <!-- Itera sobre cada usuario y muestra una fila en la tabla -->
+                        <tr class="divide-x-4 divide-white" v-for="user in $page.props.users" :key="user.id">
+                            <td class="bg-gray-200 px-6 py-4 whitespace-nowrap">{{ user.code }}</td>
+                            <td class="bg-gray-100 px-6 py-4 whitespace-nowrap">{{ user.name }}</td>
+                            <td class="bg-gray-200 px-6 py-4 whitespace-nowrap">{{ user.email }}</td>
+                            <td class="bg-gray-200 px-6 py-4 table-cell">
+                                <span v-for="adviserUser in user.get_adviser_office"
+                                    :key="adviserUser.get_estate.cod_dep"
+                                    class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 mb-2 px-2.5 py-0.5 border border-blue-400 inline-block">
+                                    {{ adviserUser.get_estate.cod_dep }} - {{ adviserUser.get_estate.dependence }}
+                                </span>
+                            </td>
+                            <td class="bg-gray-100 px-6 py-4 whitespace-nowrap text-sm font-medium">
+                                {{ user.get_role.rol }} ({{ user.get_role.id }})
+                            </td>
+                            <td class="flex flex-col bg-gray-100 px-5 py-4 whitespace-nowrap text-sm font-medium gap-2">
 
-                            <button class="border border-primary-default w-full py-1 rounded-lg flex items-center justify-center text-black hover:bg-primary-default transition-all gap-1 hover:text-white " @click="openEditUser(user)">
-                                <i class="fa-solid fa-pen-to-square"></i>
-                                Editar
-                            </button>
-                            <button v-if="user.role_id == 3"
-                                    class="border border-secondary-default w-full py-1 rounded-lg justify-center flex items-center text-balack hover:bg-secondary-default hover:text-white transition-all"
+                                <button
+                                    class="transition-all text-primary-default border border-primary-default rounded-lg px-2 py-1 hover:text-white hover:bg-primary-default hover:scale-105"
+                                    @click="openEditUser(user)">
+                                    <i class="fa-solid fa-pen-to-square"></i>
+                                    Editar
+                                </button>
+                                <button v-if="user.role_id == 3"
+                                    class="transition-all text-secondary-default border border-secondary-default rounded-lg px-2 py-1 hover:text-white hover:bg-secondary-default hover:scale-105"
                                     @click="openAssocAdviser(user)">
                                     <i class="fa-solid fa-plus"></i>
-                                Agregar Responsabilidad
-                            </button>
-                            <button class="w-full py-1 rounded-lg flex border border-red-500 items-center justify-center text-red-600 hover:bg-red-600 hover:text-white transition-all  gap-1">
-                                <i class="fa-solid fa-trash"></i>
-                                Eliminar    
-                            </button>
+                                    Agregar Responsabilidad
+                                </button>
+                                <button
+                                    class="transition-all text-red-500 border border-red-500 rounded-lg px-2 py-1 hover:text-white hover:bg-red-500 hover:scale-105">
+                                    <i class="fa-solid fa-trash"></i>
+                                    Eliminar
+                                </button>
 
-                        </td>
-                    </tr>
+                            </td>
+                        </tr>
                     </tbody>
                 </table>
             </div>
