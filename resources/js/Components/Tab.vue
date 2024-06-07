af11889b7242f209f5918b82aa20784794059f54
danifede850 <danifede850@gmail.com>
2024-06-07 10:10:19 -0500

    "Updated styles for tabs in Tab.vue component"

--- resources/js/Components/Tab.vue
+++ resources/js/Components/Tab.vue
@@ -4,11 +4,11 @@ import {ref} from "vue";
 const tab = ref(1);
 </script>
 <template>
-    <div class="bg-white shadow-md rounded-lg">
+    <div class="overflow-hidden bg-white shadow-md rounded-lg">
         <!-- Tabs -->
         <div class="flex border-b">
-            <label for="tab1" :class="[tab == 1 ? 'border-b-4' : '', 'tab-label py-2 px-4 text-gray-600 cursor-pointer hover:text-green-500']" @click="tab = 1">Indicadores de Gestión</label>
-            <label for="tab1" :class="[tab == 2 ? 'border-b-4' : '', 'tab-label py-2 px-4 text-gray-600 cursor-pointer hover:text-green-500']" @click="tab = 2">Indicadores de Prespuesto</label>
+            <label for="tab1" :class="[tab == 1 ? 'border-b-4 border-primary-700 bg-primary-default text-white' : 'hover:border-b-4 text-secondary-default hover:bg-secondary-200 hover:border-secondary-default hover:border-secpndary-default ', 'tab-label py-2 px-4 cursor-pointer  ']" @click="tab = 1">Indicadores de Gestión</label>
+            <label for="tab1" :class="[tab == 2 ? 'border-b-4 border-primary-700 bg-primary-default text-white' : 'hover:border-b-4 text-secondary-default hover:bg-secondary-200 hover:border-secondary-default hover:border-secpndary-default ', 'tab-label py-2 px-4 cursor-pointer ']" @click="tab = 2">Indicadores de Prespuesto</label>
         </div>
         <div id="content1" :class="[tab == 1 ? '' : 'hidden', 'tab-content p-4']">
             <slot name="t1"/>
