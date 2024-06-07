bfc271fcd1b627c27c29b5b6acf1a16e0652e632
danifede850 <danifede850@gmail.com>
2024-06-07 10:29:08 -0500

    fix

--- resources/js/Components/Tab.vue
+++ resources/js/Components/Tab.vue
@@ -7,8 +7,8 @@ const tab = ref(1);
     <div class="overflow-hidden bg-white shadow-md rounded-lg">
         <!-- Tabs -->
         <div class="flex border-b">
-            <label for="tab1" :class="[tab == 1 ? 'border-b-4 border-primary-700 bg-primary-default text-white' : 'hover:border-b-4 text-secondary-default hover:bg-secondary-200 hover:border-secondary-default hover:border-secpndary-default ', 'tab-label py-2 px-4 cursor-pointer  ']" @click="tab = 1">Indicadores de Gestión</label>
-            <label for="tab1" :class="[tab == 2 ? 'border-b-4 border-primary-700 bg-primary-default text-white' : 'hover:border-b-4 text-secondary-default hover:bg-secondary-200 hover:border-secondary-default hover:border-secpndary-default ', 'tab-label py-2 px-4 cursor-pointer ']" @click="tab = 2">Indicadores de Prespuesto</label>
+            <label for="tab1" :class="[tab == 1 ? 'border-b-4 border-primary-700 bg-primary-default text-white transition-all' : 'hover:border-b-4 text-secondary-default hover:bg-secondary-200 hover:border-secondary-default hover:border-secpndary-default ', 'tab-label py-2 px-4 cursor-pointer  ']" @click="tab = 1">Indicadores de Gestión</label>
+            <label for="tab1" :class="[tab == 2 ? 'border-b-4 border-primary-700 bg-primary-default text-white transition-all' : 'hover:border-b-4 text-secondary-default hover:bg-secondary-200 hover:border-secondary-default hover:border-secpndary-default ', 'tab-label py-2 px-4 cursor-pointer ']" @click="tab = 2">Indicadores de Prespuesto</label>
         </div>
         <div id="content1" :class="[tab == 1 ? '' : 'hidden', 'tab-content p-4']">
             <slot name="t1"/>
