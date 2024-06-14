54818e3f4b0a17cc0f4214b2682e72278f9aaa8b
Miguel Ángel López L. <malopezl0512@gmail.com>
2024-06-14 00:51:31 -0500

    "Added a comment to card.vue describing the component's purpose and functionality."

--- resources/js/Components/card.vue
+++ resources/js/Components/card.vue
@@ -1,3 +1,7 @@
++<!-- 
++Este componente representa una tarjeta con esquinas redondeadas, sombra y fondo blanco.
++Tiene un slot para el contenido.
++-->
 <template>
     <div class="bg-white overflow-hidden shadow-md rounded-md w-fit">
         <slot />
