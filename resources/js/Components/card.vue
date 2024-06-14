8bbfcd564d0b368a98f4d6d9f2dd0357a3488553
Miguel Ángel López L. <malopezl0512@gmail.com>
2024-06-14 08:35:40 -0500

    "Remove comments from card.vue component"

--- resources/js/Components/card.vue
+++ resources/js/Components/card.vue
@@ -1,7 +1,3 @@
-<!-- 
-+Este componente representa una tarjeta con esquinas redondeadas, sombra y fondo blanco.
-+Tiene un slot para el contenido.
-+-->
 <template>
     <div class="bg-white overflow-hidden shadow-md rounded-md w-fit">
         <slot />
